871330197d71033bfbf033e19c1d3fb70e872f84
shashankshetty1996 <smile.shashank.s.shetty1996@gmail.com>
2018-09-25 09:53:21 +0530

    fixed axios error

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {
   BrowserRouter as Router,
   Route,
   Switch,
-  Link,
+  // Link,
   NavLink
 } from 'react-router-dom';
 import './App.css';
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,12 +1,43 @@
 import React, { Component, Fragment } from 'react';
+import axios from 'axios';
 
 class Users extends Component {
+  state = {
+    userList: []
+  };
+
+  componentDidMount() {
+    axios
+      .get('http://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        this.setState({
+          userList: response.data
+        });
+      })
+      .catch(err => {
+        console.log('Error in fetching');
+      });
+  }
+
   render() {
-    return (
-      <Fragment>
-        <h1>Users Component</h1>
-      </Fragment>
-    );
+    let user;
+    if (this.state.userList.length !== 0) {
+      user = (
+        <ul className="list">
+          {this.state.userList.map(user => {
+            return (
+              <li key={user.id}>
+                {user.id} {user.username}
+              </li>
+            );
+          })}
+        </ul>
+      );
+    } else {
+      user = <h1>No data found</h1>;
+    }
+
+    return <Fragment>{user}</Fragment>;
   }
 }
 
